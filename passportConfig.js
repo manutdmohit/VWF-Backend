233eb0d69ad98566156dfec5f69abe1a24f893b4
Mohit Saud <mohit@prabidhilabs.com>
2023-11-30 13:43:18 +0545

    Added isAuthenticated instead of ensureAuth

--- passportConfig.js
+++ passportConfig.js
@@ -53,9 +53,10 @@ passport.deserializeUser(async (id, done) => {
 });
 
 exports.isAuthenticated = (req, res, next) => {
+  console.log(req.user);
   if (req.user) return next();
 
-  res.status(401).json({ message: 'Unauthorized' });
+  res.redirect('/login');
 };
 
 exports.ensureAuthenticated = (req, res, next) => {
