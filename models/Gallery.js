9222e9bf6f7edd0b8759e28988a4ec504082171c
Mohit Saud <mohit@prabidhilabs.com>
2023-12-12 14:56:16 +0545

    Add::  delete an image by asset_id

--- models/Gallery.js
+++ models/Gallery.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const cloudinary = require('cloudinary').v2; // Make sure to install the 'cloudinary' package
 
 const GallerySchema = new mongoose.Schema(
   {
@@ -9,7 +10,6 @@ const GallerySchema = new mongoose.Schema(
       },
     ],
   },
-
   {
     timestamps: true,
   }
@@ -18,4 +18,25 @@ const GallerySchema = new mongoose.Schema(
 // Create an index on the 'asset_id' field
 GallerySchema.index({ 'images.asset_id': 1 });
 
-module.exports = mongoose.model('Gallery', GallerySchema);
+// Define a method to delete an image by asset_id
+GallerySchema.methods.deleteImage = async function (asset_id) {
+  try {
+    // Delete the image from Cloudinary
+    await cloudinary.uploader.destroy(asset_id);
+
+    // Remove the image from the MongoDB document
+    this.images = this.images.filter((image) => image.asset_id !== asset_id);
+
+    // Save the updated document to the database
+    await this.save();
+
+    return { success: true, message: 'Image deleted successfully' };
+  } catch (error) {
+    console.error('Error deleting image:', error);
+    return { success: false, message: 'Error deleting image' };
+  }
+};
+
+const Gallery = mongoose.model('Gallery', GallerySchema);
+
+module.exports = Gallery;
